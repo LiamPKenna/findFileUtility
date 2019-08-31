4e61407b4836c2956c163fe099177fa8bc9490d1
LiamPKenna <liampkenna@gmail.com>
2019-08-31 09:59:07 -0700

    Removed date to make function more universal

--- findFile.js
+++ findFile.js
@@ -1,17 +1,15 @@
 const fs = require('fs');
 
-//test
-
 /**
 * @param {string} pattern pattern of file to be matched by regex
 * @param {string} year Recieves year of the file to be located
 * @param {string} dir Recieves directory name to be searched for file
 * @returns {string : null} Returns either the path of the requested file as a string, or if absent returns null
 **/
-const findFile = (pattern, year, dir) => {
+const findFile = (pattern, dir) => {
   return new Promise((resolve, reject) => {
     const files = fs.readdirSync(dir);
-    const regexp = new RegExp(`^${pattern}-${year}.*`);
+    const regexp = new RegExp(`^${pattern}.*`);
     if (files.some(file => file.match(regexp))) {
       resolve(`./${dir}/${files.find(file => file.match(regexp))}`);
     } else {
